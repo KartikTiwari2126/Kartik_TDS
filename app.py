444140083cfece1e5291bd114110cdf0e800e326
Kartikeya Tiwari <109304829+KartikTiwari2126@users.noreply.github.com>
2022-11-30 18:25:14 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.write("
+         # TDS Module 8 
+         ")
+a= st.number_input("Enter a number", min_value=1, step=1)
+if (num> 0 && num % 2==0):
+         st.write("Number is even")
+else:
+         st.write("Number is odd")
