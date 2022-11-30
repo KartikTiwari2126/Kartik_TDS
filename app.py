526e69fb235e0c70368260a86c58f904e0487333
Kartikeya Tiwari <109304829+KartikTiwari2126@users.noreply.github.com>
2022-11-30 18:49:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 st.title("Odd Even Tracker")
-num = st.number_input("Enter number",min_value = 1,step = 1)
+num = st.number_input("Enter a number !",min_value = 1,step = 1)
 if (num % 2 == 0):
-         st.write("The number is even")
+         st.write("The number is even 👍")
 else:
-         st.write("The number is odd")
-st.write("Assignment by Kartikeya Tiwari")
+         st.write("The number is odd 👍")
+st.write("A project by Kartikeya Tiwari")
